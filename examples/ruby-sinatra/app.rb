ea19b7fd4edccf81f1ae2e3621218584d6406a35
Hunter Clarke <hunter@hunterclarke.me>
2019-05-29 21:13:27 -0400

    examples

---
+++ examples/ruby-sinatra/app.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require 'planforge'
+
+PlanForge.api_key = ENV['PLANFORGE_API_KEY']
+
+get '/' do
+  customer = Orgonaut::Customer.get('cus_Epgd3XNJn0NU1P')
+  erb :index, :locals => {:customer => customer}
+end
